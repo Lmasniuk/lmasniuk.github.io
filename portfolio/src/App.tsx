21ff68c3c3cad273dab681b6f09de053e5b4f74d
Logan Masniuk <logan.masniuk@gmail.com>
2025-07-10 22:38:08 -0700

    Added some sections and styling as well as a CSS reset

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,22 +1,30 @@
 import githubLogo from './assets/githublogo.svg'
 import linkedinLogo from './assets/linkedinlogo.svg'
-import './App.css'
+import './App.scss'
 import ProjectList from './components/ProjectList/ProjectList'
 
 function App() {
 
   return (
     <main className='container'>
-      <h1>Logan Masniuk</h1>
-      <h2>Developer, Educator, Good Times Enthusiast.</h2>
-      <h2>Projects</h2>
-      <ProjectList />
+      <section className='hero'>
+
+        <h1>Logan Masniuk</h1>
+        <h2>Full Stack Developer, Educator, Good Times Enthusiast.</h2>
+
+      </section>
+      <section className="projects">
+
+        <h2>Projects</h2>
+        <ProjectList />
+      </section>
 
 
-
-      <h3>Let's Connect</h3>
-      <img src={githubLogo} />
-      <img src={linkedinLogo} />
+      <section className="connect">
+        <h2>Let's Connect</h2>
+        <img src={githubLogo} />
+        <img src={linkedinLogo} />
+      </section>
     </main >
   )
 }
--- portfolio/src/components/ProjectList/ProjectList.tsx
+++ portfolio/src/components/ProjectList/ProjectList.tsx
@@ -10,10 +10,10 @@ import atomsgamePreview from "../../assets/atoms-logo.jpeg"
 export default function ProjectList() {
     return (
         <section className="project-list">
-            <ProjectCard projectName="Shrek 5 Countdown" url="https://shrek5countdown.com" projectPreview={shrekPreview} />
             <ProjectCard projectName="Roulette Regret" url="https://rouletteregret.bet" projectPreview={roulettePreview} />
             <ProjectCard projectName="Team Genie" url="https://team-genie.netlify.app" projectPreview={teamGeniePreview} />
             <ProjectCard projectName="Atoms Game" url="https://atomsgame.netlify.app" projectPreview={atomsgamePreview} />
+            <ProjectCard projectName="Shrek 5 Countdown" url="https://shrek5countdown.com" projectPreview={shrekPreview} />
         </section>
     )
 }
