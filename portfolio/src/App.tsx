71ef22a18f460d572ae15cad3df649f163a96c2a
Logan Masniuk <logan.masniuk@gmail.com>
2025-07-11 16:20:57 -0700

    Fix alignment issue with connect links

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,32 +1,29 @@
-import githubLogo from './assets/githublogo.svg'
-import linkedinLogo from './assets/linkedinlogo.svg'
-import './App.scss'
-import ProjectList from './components/ProjectList/ProjectList'
+import githubLogo from "./assets/githublogo.svg";
+import linkedinLogo from "./assets/linkedinlogo.svg";
+import "./App.scss";
+import ProjectList from "./components/ProjectList/ProjectList";
 
 function App() {
-
   return (
-    <main className='container'>
-      <section className='hero'>
-
+    <main className="container">
+      <section className="hero">
         <h1>Logan Masniuk</h1>
         <h2>Full Stack Developer, Educator, Good Times Enthusiast.</h2>
-
       </section>
       <section className="projects">
-
         <h2>Projects</h2>
         <ProjectList />
       </section>
 
-
       <section className="connect">
         <h2>Let's Connect</h2>
-        <img src={githubLogo} />
-        <img src={linkedinLogo} />
+        <div className="connect__links">
+          <img src={githubLogo} />
+          <img src={linkedinLogo} />
+        </div>
       </section>
-    </main >
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
