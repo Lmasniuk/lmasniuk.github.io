165a3520181064fb7890ddc930c7b1cccba4e7ec
Logan Masniuk <logan.masniuk@gmail.com>
2025-07-27 18:49:45 -0700

    Add animation and add list items for skills list(better use of semantic HTML)

--- portfolio/src/components/AboutMe/AboutMe.tsx
+++ portfolio/src/components/AboutMe/AboutMe.tsx
@@ -42,148 +42,238 @@ export default function AboutMe() {
       <h2>About Me</h2>
       <p>
         Hi, I'm Logan, a full stack software developer with 10+ years of
-        experience building scalable web applications using JavaScript, Java,
-        and Python. I'm a Proven leader and educator with passion for clean
-        code, mentoring and delivering high impact solutions across agile teams.
+        experience building scalable web applications using JavaScript,
+        TypeScript, Java, and Python. I'm a Proven leader and educator with
+        passion for clean code, mentoring and delivering high impact solutions
+        across agile teams.
       </p>
       <h3 className="about-me__technologies">Technologies</h3>
       <h4 className="about-me__technologies-subset">Languages</h4>
       <ul className="about-me__technologies-list">
-        <img className="about-me__technologies-icon" src={html} alt="HTML" />
-
-        <img className="about-me__technologies-icon" src={css} alt="CSS" />
-        <img className="about-me__technologies-icon" src={sass} alt="Sass" />
-        <img
-          className="about-me__technologies-icon"
-          src={javascript}
-          alt="JavaScript"
-        />
-        <img
-          className="about-me__technologies-icon"
-          src={typescript}
-          alt="TypeScript"
-        />
-        <img
-          className="about-me__technologies-icon"
-          src={python}
-          alt="Python"
-        />
-        <img className="about-me__technologies-icon" src={java} alt="Java" />
+        <li>
+          <img className="about-me__technologies-icon" src={html} alt="HTML" />
+        </li>
+        <li>
+          <img className="about-me__technologies-icon" src={css} alt="CSS" />
+        </li>
+        <li>
+          <img className="about-me__technologies-icon" src={sass} alt="Sass" />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={javascript}
+            alt="JavaScript"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={typescript}
+            alt="TypeScript"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={python}
+            alt="Python"
+          />
+        </li>
+        <li>
+          <img className="about-me__technologies-icon" src={java} alt="Java" />
+        </li>
       </ul>
 
       <h4 className="about-me__technologies-subset">Libraries & Frameworks</h4>
       <ul className="about-me__technologies-list">
-        <img className="about-me__technologies-icon" src={react} alt="React" />
-        <img
-          className="about-me__technologies-icon"
-          src={nodejs}
-          alt="Node.js"
-        />
-        <img className="about-me__technologies-icon" src={npm} alt="npm" />
-        <img
-          className="about-me__technologies-icon"
-          src={express}
-          alt="Express"
-        />
-        <img
-          className="about-me__technologies-icon"
-          src={django}
-          alt="Django"
-        />
-        <img className="about-me__technologies-icon" src={flask} alt="Flask" />
-        <img
-          className="about-me__technologies-icon"
-          src={spring}
-          alt="Spring"
-        />
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={react}
+            alt="React"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={nodejs}
+            alt="Node.js"
+          />
+        </li>
+        <li>
+          <img className="about-me__technologies-icon" src={npm} alt="npm" />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={express}
+            alt="Express"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={django}
+            alt="Django"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={flask}
+            alt="Flask"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={spring}
+            alt="Spring"
+          />
+        </li>
       </ul>
       <h4 className="about-me__technologies-subset">
         Databases & Data Technologies
       </h4>
       <ul className="about-me__technologies-list">
-        <img className="about-me__technologies-icon" src={git} alt="Git" />
-        <img
-          className="about-me__technologies-icon"
-          src={github}
-          alt="GitHub"
-        />
-        <img
-          className="about-me__technologies-icon"
-          src={bitbucket}
-          alt="Bitbucket"
-        />
-        <img
-          className="about-me__technologies-icon"
-          src={postgres}
-          alt="Postgres"
-        />
-        <img className="about-me__technologies-icon" src={mysql} alt="MySQL" />
-        <img
-          className="about-me__technologies-icon"
-          src={sqlserver}
-          alt="SQL Server"
-        />
-        <img className="about-me__technologies-icon" src={redis} alt="Redis" />
-        <img
-          className="about-me__technologies-icon"
-          src={elasticsearch}
-          alt="Elasticsearch"
-        />
-        <img
-          className="about-me__technologies-icon"
-          src={logstash}
-          alt="Logstash"
-        />
-        <img
-          className="about-me__technologies-icon"
-          src={kibana}
-          alt="Kibana"
-        />
+        <li>
+          <img className="about-me__technologies-icon" src={git} alt="Git" />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={github}
+            alt="GitHub"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={bitbucket}
+            alt="Bitbucket"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={postgres}
+            alt="Postgres"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={mysql}
+            alt="MySQL"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={sqlserver}
+            alt="SQL Server"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={redis}
+            alt="Redis"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={elasticsearch}
+            alt="Elasticsearch"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={logstash}
+            alt="Logstash"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={kibana}
+            alt="Kibana"
+          />
+        </li>
       </ul>
       <h4 className="about-me__technologies-subset">DevOps & Cloud</h4>
       <ul className="about-me__technologies-list">
-        <img
-          className="about-me__technologies-icon"
-          src={docker}
-          alt="Docker"
-        />
-        <img className="about-me__technologies-icon" src={aws} alt="AWS" />
-        <img
-          className="about-me__technologies-icon"
-          src={githubactions}
-          alt="Github Actions"
-        />
-        <img
-          className="about-me__technologies-icon"
-          src={bamboo}
-          alt="Bamboo"
-        />
-        <img
-          className="about-me__technologies-icon"
-          src={jenkins}
-          alt="Jenkins"
-        />
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={docker}
+            alt="Docker"
+          />
+        </li>
+        <li>
+          <img className="about-me__technologies-icon" src={aws} alt="AWS" />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={githubactions}
+            alt="Github Actions"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={bamboo}
+            alt="Bamboo"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={jenkins}
+            alt="Jenkins"
+          />
+        </li>
       </ul>
       <h4 className="about-me__technologies-subset">Tools & Platforms</h4>
       <ul className="about-me__technologies-list">
-        <img
-          className="about-me__technologies-icon"
-          src={vscode}
-          alt="VS Code"
-        />
-        <img
-          className="about-me__technologies-icon"
-          src={intellij}
-          alt="IntelliJ IDEA"
-        />
-        <img className="about-me__technologies-icon" src={jira} alt="Jira" />
-        <img className="about-me__technologies-icon" src={okta} alt="Okta" />
-        <img
-          className="about-me__technologies-icon"
-          src={postman}
-          alt="Postman"
-        />
-        <img className="about-me__technologies-icon" src={figma} alt="Figma" />
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={vscode}
+            alt="VS Code"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={intellij}
+            alt="IntelliJ IDEA"
+          />
+        </li>
+        <li>
+          <img className="about-me__technologies-icon" src={jira} alt="Jira" />
+        </li>
+        <li>
+          <img className="about-me__technologies-icon" src={okta} alt="Okta" />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={postman}
+            alt="Postman"
+          />
+        </li>
+        <li>
+          <img
+            className="about-me__technologies-icon"
+            src={figma}
+            alt="Figma"
+          />
+        </li>
       </ul>
     </section>
   );
