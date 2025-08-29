1fe24ae640e5b167896817ef0eac42b16bdcd92b
Logan Masniuk <logan.masniuk@gmail.com>
2025-08-29 11:25:25 -0700

    Add image of me for hero section

--- portfolio/src/components/Hero/Hero.tsx
+++ portfolio/src/components/Hero/Hero.tsx
@@ -1,11 +1,22 @@
+import bigTree from "../../assets/big-tree.png";
 import "./Hero.scss";
+
 export default function Hero() {
   return (
     <section className="hero">
-      <h1 className="hero__title">Logan Masniuk</h1>
-      <h2 className="hero__subtitle">
-        Full Stack Developer, Good Times Enthusiast.
-      </h2>
+      <div className="hero__content">
+        <div>
+          <h1 className="hero__title">Logan Masniuk</h1>
+          <h2 className="hero__subtitle">
+            Full Stack Developer, Good Times Enthusiast.
+          </h2>
+        </div>
+        <img
+          className="hero__image"
+          src={bigTree}
+          alt="Logan standing on a wooden boardwalk next to a very large tree"
+        />
+      </div>
     </section>
   );
 }
